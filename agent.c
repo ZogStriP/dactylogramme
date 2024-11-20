060ffdc95c8d13712255a3c25ef37633c0eda4fb
zogstrip <regis@hanol.fr>
2024-11-20 14:42:54 +0100

    wip

--- agent.c
+++ agent.c
@@ -1,26 +1,7 @@
 #define _GNU_SOURCE
 
-#include <fcntl.h>
 #include <stdlib.h>
 #include <systemd/sd-bus.h>
-#include <systemd/sd-event.h>
-
-static sd_bus *bus = NULL;
-static sd_event *event = NULL;
-
-static int helper_cb(sd_event_source *s, const siginfo_t *si, void *userdata) {
-  sd_bus_message *m = userdata;
-
-  if (si->si_code == CLD_EXITED && si->si_status == EXIT_SUCCESS) {
-    sd_bus_reply_method_return(m, NULL);
-  } else {
-    sd_bus_reply_method_errorf(m, "org.freedesktop.PolicyKit1.Error.Failed", "Authentication failed");
-  }
-
-  sd_bus_message_unref(m);
-
-  return 0;
-}
 
 static int begin_authentication(sd_bus_message *m, void *userdata, sd_bus_error *error) {
   char *cookie = NULL;
@@ -28,25 +9,14 @@ static int begin_authentication(sd_bus_message *m, void *userdata, sd_bus_error 
   sd_bus_message_skip(m, "sssa{ss}");
   sd_bus_message_read(m, "s", &cookie);
 
-  sigset_t ss = { 0 };
-  sigaddset(&ss, SIGCHLD);
-  sigprocmask(SIG_BLOCK, &ss, NULL);
-
   int p[2];
   pipe(p);
 
-  pid_t pid = fork();
-
-  if (pid == 0) {
-    int dev_null = open("/dev/null", O_WRONLY);
-
+  if (fork() == 0) {
     dup2(p[0], STDIN_FILENO);
-    dup2(dev_null, STDOUT_FILENO);
-    dup2(dev_null, STDERR_FILENO);
 
     close(p[0]);
     close(p[1]);
-    close(dev_null);
 
     execlp("polkit-agent-helper-1", "polkit-agent-helper-1", getenv("USER"), NULL);
     _exit(EXIT_FAILURE);
@@ -57,56 +27,25 @@ static int begin_authentication(sd_bus_message *m, void *userdata, sd_bus_error 
   close(p[0]);
   close(p[1]);
 
-  sd_bus_message_ref(m);
-  sd_event_add_child(event, NULL, pid, WEXITED, helper_cb, m);
+  int status;
+  wait(&status);
 
-  sigprocmask(SIG_UNBLOCK, &ss, NULL);
-
-  return 1;
+  return WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS ?
+    sd_bus_reply_method_return(m, NULL) :
+    sd_bus_reply_method_errorf(m, "org.freedesktop.PolicyKit1.Error.Failed", "Authentication failed");
 }
-
-static int cancel_authentication(sd_bus_message *m, void *userdata, sd_bus_error *error) {
-  printf("CANCEL!\n");
-  return 0;
-}
-
-static int fprint_cb(sd_bus_message *m, void *userdata, sd_bus_error *error) {
-  const char *member = sd_bus_message_get_member(m);
-
-  if (strcmp(member, "VerifyFingerSelected") == 0) {
-    printf("FINGER\n"); // red
-  } else if (strcmp(member, "VerifyStatus") == 0) {
-    char *result = NULL;
-    int done;
-
-    sd_bus_message_read(m, "sb", &result, &done);
-
-    if (strcmp(result, "verify-match") == 0) {
-      printf("YES!\n"); // green
-      // sleep(500ms)
-    }
-
-    // white
-  }
-
-  return 0;
-}
-
-static const sd_bus_vtable vtable[] = {
-  SD_BUS_VTABLE_START(0),
-  SD_BUS_METHOD("BeginAuthentication", "sssa{ss}sa(sa{sv})", "", begin_authentication, SD_BUS_VTABLE_UNPRIVILEGED),
-  SD_BUS_METHOD("CancelAuthentication", "s", "", cancel_authentication, SD_BUS_VTABLE_UNPRIVILEGED),
-  SD_BUS_VTABLE_END
-};
 
 int main() {
-  sd_event_default(&event);
+  sd_bus *bus = NULL;
+
   sd_bus_open_system(&bus);
-  sd_bus_attach_event(bus, event, 0);
 
-  sd_bus_add_match(bus, NULL, "interface='net.reactivated.Fprint.Device'", fprint_cb, NULL);
-
-  sd_bus_add_object_vtable(bus, NULL, "/org/freedesktop/PolicyKit1/AuthenticationAgent", "org.freedesktop.PolicyKit1.AuthenticationAgent", vtable, NULL);
+  sd_bus_add_object_vtable(bus, 
+    NULL, 
+    "/org/freedesktop/PolicyKit1/AuthenticationAgent", 
+    "org.freedesktop.PolicyKit1.AuthenticationAgent", 
+    (sd_bus_vtable[]){ SD_BUS_VTABLE_START(0), SD_BUS_METHOD("BeginAuthentication", "sssa{ss}sa(sa{sv})", "", begin_authentication, SD_BUS_VTABLE_UNPRIVILEGED), SD_BUS_VTABLE_END }, 
+    NULL);
 
   sd_bus_call_method(bus, 
     "org.freedesktop.PolicyKit1", 
@@ -120,10 +59,8 @@ int main() {
     NULL /* locale */, 
     "/org/freedesktop/PolicyKit1/AuthenticationAgent");
 
-  sd_event_loop(event);
-
-  sd_bus_unref(bus);
-  sd_event_unref(event);
-
-  return EXIT_SUCCESS;
+  for (;;) {
+    sd_bus_wait(bus, -1);
+    sd_bus_process(bus, NULL);
+  }
 }
---
+++ luciole.c
@@ -0,0 +1,31 @@
+#define _GNU_SOURCE
+
+#include <systemd/sd-bus.h>
+
+static int fprint_cb(sd_bus_message *m, void *userdata, sd_bus_error *error) {
+  char *member = sd_bus_message_get_member(m);
+
+  if (strcmp(member, "VerifyFingerSelected") == 0) {
+    printf("FINGER\n");
+  } else if (strcmp(member, "VerifyStatus") == 0) {
+    char *result = NULL;
+    sd_bus_message_read(m, "s", &result);
+
+    if (strcmp(result, "verify-match") == 0) {
+      printf("YES!\n");
+    }
+  }
+
+  return 0;
+}
+
+int main() {
+  sd_bus *bus = NULL;
+  sd_bus_open_system(&bus);
+  sd_bus_add_match(bus, NULL, "interface='net.reactivated.Fprint.Device'", fprint_cb, NULL);
+
+  for (;;) {
+    sd_bus_wait(bus, -1);
+    sd_bus_process(bus, NULL);
+  }
+}
